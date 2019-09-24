7bbca0a2c3ee0332302f307bf4fc2eaeee36ae42
Ravindu Cooray <ravinducooray3@gmail.com>
2019-09-24 16:43:09 -0600

    test

--- assignments/test.c
+++ assignments/test.c
@@ -16,22 +16,50 @@
 
 // }
 
-
-
 int main(int argc, char ** argv){
-    int s1[1001] = {-1};
-    int s2[1001] = {-1};
+    char s1[1001];
+    char s2[1001];
     printf("To compute an LCS,\n");
     printf("Enter the first sequence: ");
-    scanf("%d", &s1[]);
+    scanf("%s", &s1[1001]);
+   
     printf("Enter the second sequence: ");
-    scanf("%d", &s2[]);
+    scanf("%s", &s2[1001]);
 
-    int s1_length = sizeof(s1)/sizeof(int);
-    int s2_length = sizeof(s2)/sizeof(int);
+    // if (scanf("%d", &s1[1001]) == 0){
+    //     printf("Error\n");
+    //     return 0;
+    // }
 
-    printf("%d and %d\n", s1[1001], s2[1001]);
+    // if (scanf("%d", &s2[1001]) == 0){
+    //     printf("Error\n");
+    //     return 0;
+    // }
+
+    printf("%s\n", s2);
+    int s1_length = strlen(s1);
+    int s2_length = strlen(s2);
+    printf("%d, first, %d", s1_length, s2_length);
+    int s1_idx = 0;
+    int repeats = 0;
+
+    for (int i = 0; i < s2_length; i++) {
+        if (s2[i] == s1[s1_idx]) {
+            s1_idx++;
+        }   
+        
+        else {s1_idx = 0;}
+
+        if (s1_idx > s1_length-1) {
+            s1_idx = 0;
+            repeats++;
+        }
+    }
+
+    printf("complete");
+    printf("\n%d and %d\n", s1[1000], s2[1000]);
     printf("%d and %d\n", s1_length, s2_length);
-
+    printf("repats are : %d", repeats);
+    return 0;
 }
 
